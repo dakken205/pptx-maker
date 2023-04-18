db03934dd30b12a695c12a026c1710ffc2572e58
dike-okayama <lawliet.d.k.m@gmail.com>
2023-04-18 20:46:09 +0900

    Fixed to work for now.

--- backend/app.py
+++ backend/app.py
@@ -5,58 +5,73 @@ from pptx import Presentation
 from pptx.util import Inches
 import io
 
-app = Flask(__name__, static_folder="./../frontend/dist/static", template_folder="./../frontend/dist")
+app = Flask(__name__,
+            static_folder="./../frontend/dist/static",
+            template_folder="./../frontend/dist")
 
 app.config['JSON_AS_ASCII'] = False
 CORS(app)
 # CORS(app, resources={r"/*": {"origins": ["http://localhost:8080"]}})
 
+
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,DELETE,OPTIONS')
     return response
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/')
 def index(path):
     return render_template("index.html")
 
+
 @app.route('/generate', methods=['POST'])
 def generate():
+    global ppt_file
     # try:
-        if request.method == 'POST':
-            data = request.get_json()
-            hasInfo = bool(len(data['info_contents']))
+    if request.method == 'POST':
+        data = request.get_json()
+        hasInfo = bool(len(data['info_contents']))
 
-            prs = Presentation()
-            prs.slide_width = Inches(16 * 5 / 6)
-            prs.slide_height = Inches(9 * 5 / 6)
+        prs = Presentation()
+        prs.slide_width = Inches(16 * 5 / 6)
+        prs.slide_height = Inches(9 * 5 / 6)
 
-            add_start_slide(prs, data['datefmt'])
-            add_outline_slide(prs, hasInfo)
-            add_department_slide(prs, data['departments_contents'])
-            add_info_share(prs, data['info_contents'])
-            
-            # powerpointファイルをメモリ上に保存する
-            ppt_file = io.BytesIO()
-            prs.save(ppt_file)
-            
-            # ダウンロード用URLを返す
-            download_url = request.host_url + 'download'
-            return jsonify({'download_url': download_url})
-        else:
-            return make_response(jsonify({'result' : 'Invalid method.'}))
+        add_start_slide(prs, data['datefmt'])
+        add_outline_slide(prs, hasInfo)
+        add_department_slide(prs, data['departments_contents'])
+        add_info_share(prs, data['info_contents'])
+
+        # powerpointファイルをメモリ上に保存する
+        ppt_file = io.BytesIO()
+        prs.save(ppt_file)
+        ppt_file.seek(0)
+
+        # ダウンロード用URLを返す
+        download_url = request.host_url + 'download'
+        return jsonify({'download_url': download_url})
+    else:
+        return make_response(jsonify({'result': 'Invalid method.'}))
     # except:
-        # return make_response(jsonify({'result' : 'Something error occured in server.'}))
+    # return make_response(jsonify({'result' : 'Something error occured in server.'}))
 
-@app.route('/download', methods=['POST'])
+
+@app.route('/download', methods=['GET'])
 def download_ppt():
     # PowerPointファイルをダウンロードする
-    ppt_file = generate()
-    return send_file(ppt_file, as_attachment=True)
+    # ppt_file = generate()
+    return send_file(
+        ppt_file,
+        as_attachment=True,
+        attachment_filename="sample_presentation.pptx",  # TODO: 適切な名前に変更
+        mimetype=
+        "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+    )
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
