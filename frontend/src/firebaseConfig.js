e3acd9e11a62008ad667cfd7c712dded44f879a3
llillillj <83072738+llillillj@users.noreply.github.com>
2023-04-21 20:21:18 +0900

    firebaseConfig.jsのignore

--- frontend/src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDd4tWT3smkQg67uQfMEj4FEpYqQYuE734",
-    authDomain: "regular-meeting-pptx-maker.firebaseapp.com",
-    projectId: "regular-meeting-pptx-maker",
-    storageBucket: "regular-meeting-pptx-maker.appspot.com",
-    messagingSenderId: "552324781704",
-    appId: "1:552324781704:web:3271bca2cc16eb3144313d"
-  };
