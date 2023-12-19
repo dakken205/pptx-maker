d732213883b4fe3711ff3aaa5b99e0b77dc3b26c
dike-okayama <lawliet.d.k.m@gmail.com>
2023-12-19 22:26:18 +0900

    Update to read environment variables from .env.local

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,18 +5,25 @@ import store from './store'
 import { Loading, Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
 import axios from 'axios'
-import { firebaseConfig } from './firebaseConfig'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { initializeApp } from 'firebase/app'
 
-const firebase = initializeApp(firebaseConfig)
+const firebase = initializeApp({
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+})
 const firestore = getFirestore()
 const auth = getAuth()
 const vueObj = createApp(App)
 
 vueObj.config.globalProperties.$axios = axios.create({
-    baseURL: 'http://127.0.0.1:5000/'
+    baseURL: process.env.VUE_API_URL,
 })
 vueObj.config.globalProperties.$firebase = firebase
 vueObj.config.globalProperties.$firestore = firestore
