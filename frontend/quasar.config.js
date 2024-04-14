2cd5fca7b95064306da8a5408846ea79153f9f80
Daiki Okayama <lawliet.d.k.m@gmail.com>
2024-04-14 16:08:18 +0900

    Configure deployment settings

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/pptx-maker',
       // analyze: true,
       // env: {},
       // rawDefine: {}
