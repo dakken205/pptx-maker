0b2f27037d8a36c5d3d41232b2ec8eceb93c2889
Daiki Okayama <lawliet.d.k.m@gmail.com>
2024-04-16 20:35:13 +0900

    Fix bug with no redirect to sign-in page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,4 +2,28 @@
   <router-view />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useQuasar } from 'quasar';
+import { useRouter } from 'vue-router';
+import { auth } from 'boot/firebase';
+import { browserLocalPersistence } from 'firebase/auth';
+
+const $q = useQuasar();
+const router = useRouter();
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  auth.setPersistence(browserLocalPersistence).then(() => {
+    if (requiresAuth && !auth.currentUser) {
+      $q.notify({
+        color: 'negative',
+        position: 'top',
+        message: 'ログインが必要です',
+      });
+      next('/login');
+    } else {
+      next();
+    }
+  });
+});
+</script>
--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -3,11 +3,13 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
+    meta: { requiresAuth: true },
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
   {
     path: '/login',
+    meta: { requiresAuth: false },
     component: () => import('pages/LoginPage.vue'),
   },
   // Always leave this as last one,
